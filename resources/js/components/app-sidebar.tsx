c12cf5491b1cc48173ad34024ef79a40b338c8be
Kholil Abdi Prasetiyo <kholilapras@gmail.com>
2025-06-20 22:56:03 +0700

    update - sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, HelpCircle, LayoutGrid, List } from 'lucide-react';
+import { HelpCircle, LayoutGrid, List, ScrollText } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -16,7 +16,7 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Cashier',
         href: '/cashier',
-        icon: List,
+        icon: ScrollText,
     },
     {
         title: 'Product',
